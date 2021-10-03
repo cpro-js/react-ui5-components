efcba43432d39d60acfe2373d27bdd342a36f471
texttechne <gedankengut@gmx.de>
2021-10-03 16:25:00 +0200

    feat(storybook): add docs & useState for stories

--- packages/form/.storybook/main.js
+++ packages/form/.storybook/main.js
@@ -1,4 +1,20 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-docs",
+  ],
+  /* typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldRemoveUndefinedFromOptional: false,
+      shouldExtractValuesFromUnion: true,
+      shouldExtractLiteralValuesFromEnum: true,
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  }, */
 };
