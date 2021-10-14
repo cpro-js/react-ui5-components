fcb212776caabf2b00e020da88226223cf954d2b
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2021-10-14 12:56:34 +0200

    fix: forward refs to web component

--- packages/form/src/field/TextInputField.tsx
+++ packages/form/src/field/TextInputField.tsx
@@ -1,8 +1,8 @@
 import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";
 
 import { ValueState } from "@ui5/webcomponents-react";
-import { FC, useMemo } from "react";
-import { Controller } from "react-hook-form";
+import { FC, forwardRef, useImperativeHandle, useMemo, useRef } from "react";
+import { useController } from "react-hook-form";
 
 import { TextInput, TextInputProps } from "../component/TextInput";
 import { useI18nValidationError } from "../i18n/FormI18n";
@@ -17,13 +17,10 @@ export type TextInputFieldProps = Omit<
     name: string;
   };
 
-export const TextInputField: FC<TextInputFieldProps> = ({
-  name,
-  required,
-  minLength,
-  maxLength,
-  ...props
-}) => {
+export const TextInputField: FC<TextInputFieldProps> = forwardRef<
+  HTMLInputElement | undefined,
+  TextInputFieldProps
+>(({ name, required, minLength, maxLength, ...props }, ref) => {
   const rules: Partial<FormFieldValidation> = useMemo(
     () => ({
       required,
@@ -35,46 +32,50 @@ export const TextInputField: FC<TextInputFieldProps> = ({
 
   const getValidationErrorMessage = useI18nValidationError(name, rules);
 
+  const { field, fieldState } = useController({
+    name: name,
+    rules,
+  });
+
+  // store input ref for intenral usage
+  const inputRef = useRef<HTMLInputElement>();
+  // forward outer ref to stored internal input ref
+  useImperativeHandle(ref, () => inputRef.current);
+  // forward field ref to stored internal input ref
+  useImperativeHandle(field.ref, () => inputRef.current);
+
+  // use empty string to reset value, undefined will be ignored by web component
+  const value = field.value === undefined ? "" : field.value;
+
+  // get error message (Note: undefined fallbacks to default message of ui5 component)
+  const errorMessage = hasError(fieldState.error)
+    ? getValidationErrorMessage(fieldState.error, field.value)
+    : undefined;
+
   return (
-    <Controller<any>
-      name={name}
-      rules={rules}
-      render={({ field, fieldState }) => {
-        // use empty string to reset value, undefined will be ignored by web component
-        const value = field.value === undefined ? "" : field.value;
-
-        // get error message (Note: undefined fallbacks to default message of ui5 component)
-        const errorMessage = hasError(fieldState.error)
-          ? getValidationErrorMessage(fieldState.error, field.value)
-          : undefined;
-
-        return (
-          <TextInput
-            {...props}
-            ref={field.ref}
-            name={field.name}
-            value={value}
-            onChange={field.onChange}
-            onBlur={field.onBlur}
-            valueState={
-              hasError(fieldState.error) ? ValueState.Error : ValueState.None
-            }
-            valueStateMessage={
-              errorMessage != null && (
-                <div slot="valueStateMessage">{errorMessage}</div>
-              )
-            }
-            required={required}
-            maxlength={
-              maxLength != null
-                ? typeof maxLength === "number"
-                  ? maxLength
-                  : maxLength.value
-                : undefined
-            }
-          />
-        );
-      }}
+    <TextInput
+      {...props}
+      ref={inputRef}
+      name={field.name}
+      value={value}
+      onChange={field.onChange}
+      onBlur={field.onBlur}
+      valueState={
+        hasError(fieldState.error) ? ValueState.Error : ValueState.None
+      }
+      valueStateMessage={
+        errorMessage != null && (
+          <div slot="valueStateMessage">{errorMessage}</div>
+        )
+      }
+      required={required}
+      maxlength={
+        maxLength != null
+          ? typeof maxLength === "number"
+            ? maxLength
+            : maxLength.value
+          : undefined
+      }
     />
   );
-};
+});
