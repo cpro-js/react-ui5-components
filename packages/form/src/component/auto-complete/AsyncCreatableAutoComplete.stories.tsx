1ba84ccfd33ffdbe93cfbda0e43851b299b7e0a7
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2021-10-21 20:29:17 +0200

    fix: async creatable auto complete

---
+++ packages/form/src/component/auto-complete/AsyncCreatableAutoComplete.stories.tsx
@@ -0,0 +1,92 @@
+import "@ui5/webcomponents-icons/dist/add";
+import "@ui5/webcomponents-icons/dist/refresh";
+import "@ui5/webcomponents-icons/dist/search";
+import "@ui5/webcomponents-icons/dist/sort";
+import "@ui5/webcomponents-icons/dist/value-help.js";
+
+import { Meta, Story } from "@storybook/react";
+import { ValueState } from "@ui5/webcomponents-react";
+
+import {
+  AsyncCreatableAutocomplete,
+  AsyncCreatableAutocompleteProps,
+} from "./AsyncCreatableAutocomplete";
+import { COUNTRIES, SEARCH_COUNTRIES } from "./AutoComplete-storyData";
+import { DefaultAutoCompleteOption } from "./internal/CoreAutocomplete";
+
+const Template: Story<
+  AsyncCreatableAutocompleteProps<DefaultAutoCompleteOption>
+> = ({ ...props }) => {
+  return <AsyncCreatableAutocomplete {...props} onSearch={SEARCH_COUNTRIES} />;
+};
+
+export const Standard = Template.bind({});
+Standard.args = { value: undefined, onSearch: SEARCH_COUNTRIES };
+
+export const Empty = Template.bind({});
+Standard.args = { value: undefined };
+
+export const Prefilled = Template.bind({});
+Prefilled.args = {
+  ...Standard.args,
+  value: COUNTRIES[1].value,
+  defaultItems: [COUNTRIES[1]],
+};
+
+export const PrefilledWithValueOnly = Template.bind({});
+PrefilledWithValueOnly.args = {
+  ...Standard.args,
+  value: COUNTRIES[1].value,
+};
+
+export const MinCharacters = Template.bind({});
+MinCharacters.args = { ...Standard.args, minCharsForSearch: 3 };
+
+export const CustomLabelProp = Template.bind({});
+CustomLabelProp.args = { ...Prefilled.args, itemLabel: "withUmlaut" };
+
+export const CustomLabelFunction = Template.bind({});
+CustomLabelFunction.args = {
+  ...Prefilled.args,
+  itemLabel: (country) => `ISO code: ${country.value} => ${country.label}`,
+};
+
+export const CustomValueProp = Template.bind({});
+CustomValueProp.args = { ...Prefilled.args, itemValue: "label" };
+
+export const CustomValueFunction = Template.bind({});
+CustomValueFunction.args = {
+  ...Prefilled.args,
+  value: COUNTRIES[1].value,
+  itemValue: (country) => `${country.label} (${country.value})`,
+};
+
+export const RenderSuggestion = Template.bind({});
+RenderSuggestion.args = {
+  ...Standard.args,
+  getItemProps: (country) => ({
+    text: country.label,
+    // @ts-ignore
+    description: country.withUmlaut as string,
+    icon: "add",
+    // iconEnd: "info",
+    info: "Infozzz",
+    infoState: ValueState.Success,
+  }),
+};
+
+export default {
+  title: "form/component/AsyncCreatableAutocomplete",
+  component: AsyncCreatableAutocomplete,
+  argTypes: {
+    onValueCreate: {
+      action: "onValueCreate",
+    },
+    onInputChange: {
+      action: "onInputChange",
+    },
+    onValueChange: {
+      action: "onValueChange",
+    },
+  },
+} as Meta;
---
+++ packages/form/src/component/auto-complete/AsyncCreatableAutocomplete.tsx
@@ -0,0 +1,55 @@
+import { Simplify } from "type-fest";
+
+import { DefaultAutoCompleteOption } from "../AutoCompleteModel";
+import {
+  UseAsyncAdditionalProps,
+  UseAsyncManagedPropKeys,
+  useAsync,
+} from "./hooks/useAsync";
+import {
+  UseCreatableAdditionalProps,
+  useCreatable,
+} from "./hooks/useCreatable";
+import { useInputState } from "./hooks/useInputState";
+import {
+  UseItemAdditionalProps,
+  UseItemModelManagedPropKeys,
+  useItemModel,
+} from "./hooks/useItemModel";
+import {
+  CoreAutocomplete,
+  CoreAutocompleteProps,
+} from "./internal/CoreAutocomplete";
+
+export type AsyncCreatableAutocompleteProps<
+  TModel = DefaultAutoCompleteOption
+> = Simplify<
+  Omit<
+    CoreAutocompleteProps<TModel>,
+    UseAsyncManagedPropKeys | UseItemModelManagedPropKeys
+  > &
+    UseAsyncAdditionalProps<TModel> &
+    UseItemAdditionalProps<TModel> &
+    UseCreatableAdditionalProps<TModel>
+>;
+
+export const AsyncCreatableAutocomplete = (
+  props: AsyncCreatableAutocompleteProps
+) => {
+  const itemModelProps = useItemModel<DefaultAutoCompleteOption, typeof props>(
+    props
+  );
+  const asyncProps = useAsync<DefaultAutoCompleteOption, typeof itemModelProps>(
+    itemModelProps
+  );
+  const stateProps = useInputState<
+    DefaultAutoCompleteOption,
+    typeof asyncProps
+  >(asyncProps);
+  const creatableProps = useCreatable<
+    DefaultAutoCompleteOption,
+    typeof stateProps
+  >(stateProps);
+
+  return <CoreAutocomplete {...creatableProps} />;
+};
