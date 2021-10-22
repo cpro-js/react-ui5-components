d6dfd1aeaf05f4c8368aefa7afa9bcd367b4708d
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2021-10-22 17:11:26 +0200

    feat: async creatable autocomplete field

---
+++ packages/form/src/field/autocomplete/AsyncCreatableAutocompleteField.stories.tsx
@@ -0,0 +1,116 @@
+import { Story } from "@storybook/react";
+import useState from "storybook-addon-state";
+
+import {
+  COUNTRIES,
+  SEARCH_COUNTRIES,
+} from "../../component/auto-complete/AutoComplete-storyData";
+import { DefaultAutoCompleteOption } from "../../component/auto-complete/internal/CoreAutocomplete";
+import { FormController, FormControllerProps } from "../../form/FormController";
+import { FormI18nProvider } from "../../i18n/FormI18n";
+import { FormActions } from "../types";
+import {
+  AsyncCreatableAutocompleteField,
+  AsyncCreatableAutocompleteFieldProps,
+} from "./AsyncCreatableAutocompleteField";
+
+interface FormData {
+  item?: string | number;
+}
+
+const Template: Story<
+  FormControllerProps<FormData> &
+    AsyncCreatableAutocompleteFieldProps<DefaultAutoCompleteOption>
+> = (args, context) => {
+  const { initialValues, ...props } = args;
+
+  const [submittedValues, setSubmittedValues] = useState(
+    `${context.name}_submittedValues`,
+    {}
+  );
+  const NoData = <p>No submitted data yet!</p>;
+
+  const onSubmit = (values: FormData, actions: FormActions<FormData>) => {
+    setSubmittedValues(values);
+  };
+
+  return (
+    <FormController<FormData> {...{ initialValues, onSubmit }}>
+      <AsyncCreatableAutocompleteField {...props} name="item" />
+      <div>
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+      </div>
+
+      <h2>Submitted Values</h2>
+      {!Object.keys(submittedValues).length
+        ? NoData
+        : JSON.stringify(submittedValues)}
+    </FormController>
+  );
+};
+
+const I18nTemplate: Story<
+  FormControllerProps<FormData> &
+    AsyncCreatableAutocompleteFieldProps<DefaultAutoCompleteOption>
+> = (args, context) => {
+  return (
+    <FormI18nProvider
+      getValidationErrorMessage={({ name }, error) => {
+        return `Field '${name}' has Error '${
+          error.type
+        }'. Original error message: ${error.message || "---"}`;
+      }}
+    >
+      {Template(args, context)}
+    </FormI18nProvider>
+  );
+};
+
+export const Standard = Template.bind({});
+Standard.args = { onSearch: SEARCH_COUNTRIES };
+
+export const Prefilled = Template.bind({});
+Prefilled.args = {
+  ...Standard.args,
+  initialItems: [COUNTRIES[1]],
+  initialValues: {
+    item: COUNTRIES[1].value,
+  },
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  ...Prefilled.args,
+  disabled: true,
+};
+
+export const Readonly = Template.bind({});
+Readonly.args = {
+  ...Prefilled.args,
+  readonly: true,
+};
+
+export const ValidationRequired = Template.bind({});
+ValidationRequired.args = {
+  ...Standard.args,
+  required: true,
+};
+
+export const ValidationTranslationRequired = I18nTemplate.bind({});
+ValidationTranslationRequired.args = {
+  ...ValidationRequired.args,
+};
+
+export default {
+  title: "Form/Field/Autocomplete/AsyncCreatableAutocompleteField",
+  component: AsyncCreatableAutocompleteField,
+  argTypes: {
+    onSubmit: {
+      action: "submit",
+    },
+    onValueCreate: {
+      action: "onValueCreate",
+    },
+  },
+};
---
+++ packages/form/src/field/autocomplete/AsyncCreatableAutocompleteField.tsx
@@ -0,0 +1,79 @@
+import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";
+
+import { ValueState } from "@ui5/webcomponents-react";
+import { useImperativeHandle, useMemo, useRef } from "react";
+import { useController } from "react-hook-form";
+
+import {
+  AsyncCreatableAutocomplete,
+  AsyncCreatableAutocompleteProps,
+} from "../../component/auto-complete/AsyncCreatableAutocomplete";
+import { useI18nValidationError } from "../../i18n/FormI18n";
+import { FormFieldValidation } from "../types";
+import { hasError } from "../util";
+
+export type AsyncCreatableAutocompleteFieldProps<T> = Omit<
+  AsyncCreatableAutocompleteProps<T>,
+  "name" | "value" | "inputValue" | "onChange" | "onValueChange" | "onBlur"
+> &
+  Pick<FormFieldValidation, "required"> & {
+    name: string;
+  };
+
+export const AsyncCreatableAutocompleteField = <T extends Object>({
+  name,
+  required,
+  ...props
+}: AsyncCreatableAutocompleteFieldProps<T>) => {
+  const rules: Partial<FormFieldValidation> = useMemo(
+    () => ({
+      required,
+    }),
+    [required]
+  );
+
+  const getValidationErrorMessage = useI18nValidationError(name, rules);
+
+  const { field, fieldState } = useController({
+    name: name,
+    rules,
+  });
+
+  // store input ref for intenral usage
+  const inputRef = useRef<HTMLInputElement>();
+  // forward outer ref to custom element
+  // useImperativeHandle(forwardedRef, () => ({
+  //   focus() {
+  //     if (inputRef.current != null) {
+  //       inputRef.current.focus();
+  //     }
+  //   },
+  // }));
+  // forward field ref to stored internal input ref
+  useImperativeHandle(field.ref, () => inputRef.current);
+
+  // get error message (Note: undefined fallbacks to default message of ui5 component)
+  const errorMessage = hasError(fieldState.error)
+    ? getValidationErrorMessage(fieldState.error, field.value)
+    : undefined;
+
+  return (
+    <AsyncCreatableAutocomplete<T>
+      {...props}
+      ref={field.ref}
+      name={field.name}
+      value={field.value}
+      onValueChange={field.onChange}
+      valueState={
+        hasError(fieldState.error) ? ValueState.Error : ValueState.None
+      }
+      valueStateMessage={
+        errorMessage != null && (
+          <div slot="valueStateMessage">{errorMessage}</div>
+        )
+      }
+      onBlur={field.onBlur}
+      required={required}
+    />
+  );
+};
