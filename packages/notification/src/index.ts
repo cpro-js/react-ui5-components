53e75eacc352482eb324ce2ed485139508ab9a6e
texttechne <gedankengut@gmx.de>
2022-06-02 13:07:09 +0200

    refactor(react-ui5-notification): createNotificationRegistry with same signature as I18nService

--- packages/notification/src/index.ts
+++ packages/notification/src/index.ts
@@ -6,9 +6,8 @@ import { NotificationStore } from "./NotificationStore";
 export { NotificationRenderer } from "./NotificationRenderer";
 export { NotificationService };
 
-export const createNotificationRegistry: AsyncModuleRegistry = async (
-  container
-) => {
-  container.bindSingleton(NotificationStore);
-  container.bindSingleton(NotificationService);
-};
+export const createNotificationRegistry: () => AsyncModuleRegistry =
+  () => async (container) => {
+    container.bindSingleton(NotificationStore);
+    container.bindSingleton(NotificationService);
+  };
