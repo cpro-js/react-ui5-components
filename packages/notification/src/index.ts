5b7247a4a526563ec3af0ad899e6bb257b312ab0
texttechne <gedankengut@gmx.de>
2022-06-02 03:08:48 +0200

    feat(react-ui5-notification): expose NotificationRenderer and NotificationService

---
+++ packages/notification/src/index.ts
@@ -0,0 +1,4 @@
+export type { UserErrorModel } from "./NotificationModel";
+export { NotificationRenderer } from "./NotificationRenderer";
+export { NotificationService } from "./NotificationService";
+export { NotificationStore } from "./NotificationStore";
