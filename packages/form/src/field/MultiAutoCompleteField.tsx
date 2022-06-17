abfa5ba42787e17a614c6a5b3723d91e3d3c3a38
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2022-06-18 01:08:48 +0200

    fix(react-ui5-webcomponents-form): forward ref for MultiAutoComplete

--- packages/form/src/field/MultiAutoCompleteField.tsx
+++ packages/form/src/field/MultiAutoCompleteField.tsx
@@ -1,18 +1,28 @@
 import "../form/formSupport";
 
 import { ValueState } from "@ui5/webcomponents-react";
-import { FC, useMemo } from "react";
-import { Controller } from "react-hook-form";
+import { MultiComboBoxDomRef } from "@ui5/webcomponents-react/webComponents/MultiComboBox";
+import {
+  MutableRefObject,
+  ReactElement,
+  Ref,
+  forwardRef,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+} from "react";
+import { useController } from "react-hook-form";
 
+import { DefaultAutoCompleteOption } from "../component/AutoCompleteModel";
 import {
   MultiAutoComplete,
   MultiAutoCompleteProps,
 } from "../component/MultiAutoComplete";
 import { useI18nValidationError } from "../i18n/FormI18n";
-import { FormFieldValidation } from "./types";
+import { FormFieldElement, FormFieldValidation } from "./types";
 import { hasError } from "./util";
 
-export type MultiAutoCompleteFieldProps<T> = Omit<
+export type MultiAutoCompleteFieldProps<T = DefaultAutoCompleteOption> = Omit<
   MultiAutoCompleteProps<T>,
   "name" | "values" | "onChange" | "onSelectionChange" | "onBlur"
 > &
@@ -20,11 +30,10 @@ export type MultiAutoCompleteFieldProps<T> = Omit<
     name: string;
   };
 
-export const MultiAutoCompleteField = <T extends Object>({
-  name,
-  required,
-  ...props
-}: MultiAutoCompleteFieldProps<T>) => {
+export const MultiAutoCompleteField = forwardRef<
+  FormFieldElement,
+  MultiAutoCompleteFieldProps
+>(({ name, required, ...props }, forwardedRef) => {
   const rules: Partial<FormFieldValidation> = useMemo(
     () => ({
       required,
@@ -34,36 +43,54 @@ export const MultiAutoCompleteField = <T extends Object>({
 
   const getValidationErrorMessage = useI18nValidationError(name, rules);
 
-  return (
-    <Controller<any>
-      name={name}
-      rules={rules}
-      render={({ field, fieldState }) => {
-        // get error message (Note: undefined fallbacks to default message of ui5 component)
-        const errorMessage = hasError(fieldState.error)
-          ? getValidationErrorMessage(fieldState.error, field.value)
-          : undefined;
+  const { field, fieldState } = useController({
+    name: name,
+    rules,
+  });
 
-        return (
-          <MultiAutoComplete
-            {...props}
-            ref={field.ref}
-            name={field.name}
-            values={field.value}
-            onChange={(_, value) => field.onChange(value)}
-            valueState={
-              hasError(fieldState.error) ? ValueState.Error : ValueState.None
-            }
-            valueStateMessage={
-              errorMessage != null && (
-                <div slot="valueStateMessage">{errorMessage}</div>
-              )
-            }
-            onBlur={field.onBlur}
-            required={required}
-          />
-        );
-      }}
+  // store input ref for intenral usage
+  const internalRef = useRef<MultiAutoComplete<DefaultAutoCompleteOption>>();
+  // forward outer ref to custom element
+  useImperativeHandle(forwardedRef, () => ({
+    focus() {
+      if (internalRef.current != null) {
+        internalRef.current.focus();
+      }
+    },
+  }));
+  // forward field ref to stored internal input ref
+  useImperativeHandle(field.ref, () => internalRef.current);
+
+  // get error message (Note: undefined fallbacks to default message of ui5 component)
+  const errorMessage = hasError(fieldState.error)
+    ? getValidationErrorMessage(fieldState.error, field.value)
+    : undefined;
+
+  return (
+    <MultiAutoComplete
+      {...props}
+      ref={
+        internalRef as MutableRefObject<
+          MultiAutoComplete<DefaultAutoCompleteOption>
+        >
+      }
+      name={field.name}
+      values={field.value}
+      onChange={(_, value) => field.onChange(value)}
+      valueState={
+        hasError(fieldState.error) ? ValueState.Error : ValueState.None
+      }
+      valueStateMessage={
+        errorMessage != null && (
+          <div slot="valueStateMessage">{errorMessage}</div>
+        )
+      }
+      onBlur={field.onBlur}
+      required={required}
     />
   );
-};
+}) as <T = DefaultAutoCompleteOption>(
+  p: MultiAutoCompleteFieldProps<T> & {
+    ref?: Ref<FormFieldElement | undefined>;
+  }
+) => ReactElement;
