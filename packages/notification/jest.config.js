193042e026722bf0a82f37680324990373bc40c5
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2022-07-24 19:13:02 +0200

    chore: store jest snapshots next to test file

--- packages/notification/jest.config.js
+++ packages/notification/jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   testEnvironment: "node",
-  setupFiles: [require.resolve("./jest.setupTests")],
+  snapshotResolver: require.resolve("./.jest/snapshotResolver"),
+  setupFiles: [require.resolve("./.jest/setupTestsAfterEnv")],
   transformIgnorePatterns: ["node_modules/(?!(@ui5|@react-hook|lit-html))"],
   transform: {
     "^.+\\.(t|j)sx?$": [
