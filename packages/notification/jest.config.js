a38d113608dec41a05f21259986a5faba7bba382
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2022-07-24 19:09:41 +0200

    chore: use swc as compiler for jest

--- packages/notification/jest.config.js
+++ packages/notification/jest.config.js
@@ -1,6 +1,30 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: [require.resolve("./jest.setupTests")],
+  transformIgnorePatterns: ["node_modules/(?!(@ui5|@react-hook|lit-html))"],
+  transform: {
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        sourceMaps: true,
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+            decorators: true,
+            dynamicImport: false,
+          },
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
 };
