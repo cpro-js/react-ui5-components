42fcc0ea298d4fb3159d1b6a64a798805dbb0b02
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2022-07-30 18:36:35 +0200

    test: use utc timezone for jest

--- packages/notification/jest.config.js
+++ packages/notification/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   displayName: require("./package.json").name,
   testEnvironment: "jest-environment-jsdom",
   snapshotResolver: require.resolve("./.jest/snapshotResolver"),
+  globalSetup: require.resolve("./.jest/globalSetup"),
   setupFilesAfterEnv: [require.resolve("./.jest/setupTestsAfterEnv")],
   transformIgnorePatterns: ["node_modules/(?!(@ui5|@react-hook|lit-html))"],
   transform: {
