a10490875dce3996a36911d518329ebfdd06b88d
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2023-11-22 17:33:44 +0100

    fix(react-ui5-webcomponents-form): select story

--- packages/form/src/component/Select.stories.test.tsx
+++ packages/form/src/component/Select.stories.test.tsx
@@ -1,11 +1,11 @@
-import { Story } from "@storybook/react";
+import { StoryFn } from "@storybook/react";
 import { composeStories } from "@storybook/testing-react";
 
 import { renderWithDefine } from "../test/render";
 import * as stories from "./Select.stories";
 
 const testCases = Object.values(composeStories(stories)).map(
-  (StoryFn: Story) => [StoryFn.storyName!, StoryFn]
+  (StoryFn: StoryFn) => [StoryFn.storyName!, StoryFn]
 );
 // Batch snapshot testing
 test.each(testCases)("Renders %s story", async (_storyName, StoryFn) => {
