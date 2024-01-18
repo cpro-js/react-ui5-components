7a4fb6138c320b5b7055e078d8277d00654b60b9
Jan-André Zinser <jan-andre.zinser@cpro-conlog.com>
2024-01-18 13:40:33 +0100

    chore: vite build setup

--- packages/form/vite.config.ts
+++ packages/form/vite.config.ts
@@ -15,7 +15,15 @@ export default defineConfig({
     },
   },
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [
+          "babel-plugin-transform-typescript-metadata",
+          ["@babel/plugin-proposal-decorators", { legacy: true }],
+          ["@babel/plugin-proposal-class-properties", { loose: true }],
+        ],
+      },
+    }),
     dts({
       rollupTypes: true,
     }),
