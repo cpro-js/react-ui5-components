e759e05be205dbf43551934c88233f781ef7b446
nwoellgens <nico.woellgens@cpro-conlog.com>
2024-07-09 10:54:48 +0200

    docs: testing comment

--- packages/form/src/component/Button.tsx
+++ packages/form/src/component/Button.tsx
@@ -53,7 +53,12 @@ export interface ButtonPropsTest
  *  <a href="https://sap.github.io/ui5-webcomponents-react/?path=/docs/inputs-button--docs" target="_blank">Ui5 Button</a> adding
  * specific behavior related to form submission and reset functionality.
  */
-export const Button: FC<ButtonProps> = ({ form, type, onClick, ...others }) => {
+export const Button: FC<ButtonPropsTest> = ({
+  form,
+  type,
+  onClick,
+  ...others
+}) => {
   const handleClick = useCallback(
     (event: MouseEvent<ButtonDomRef>) => {
       if (onClick != null) {
