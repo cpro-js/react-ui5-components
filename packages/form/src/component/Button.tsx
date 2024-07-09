08f86360cc020f4786db5afe63418e96e115da32
nwoellgens <nico.woellgens@cpro-conlog.com>
2024-07-09 12:44:19 +0200

    fix: adding children attribute to fix story error

--- packages/form/src/component/Button.tsx
+++ packages/form/src/component/Button.tsx
@@ -34,6 +34,7 @@ export type ButtonProps = SharedHtmlProps &
     | "tooltip"
     | "accessibleName"
     | "accessibleNameRef"
+    | "children"
   > & {
     /** Optional property, use when refering to an external form by ID */
     form?: string;
@@ -41,24 +42,11 @@ export type ButtonProps = SharedHtmlProps &
     type?: "button" | "submit" | "reset";
   };
 
-export interface ButtonPropsTest
-  extends Omit<ButtonPropTypes, "submits" | "type"> {
-  /** Optional property, use when refering to an external form by ID */
-  form?: string;
-  /** Optional property, defines type of button */
-  type?: "button" | "submit" | "reset";
-}
-
 /** `Button` wrapper for
  *  <a href="https://sap.github.io/ui5-webcomponents-react/?path=/docs/inputs-button--docs" target="_blank">Ui5 Button</a> adding
  * specific behavior related to form submission and reset functionality.
  */
-export const Button: FC<ButtonPropsTest> = ({
-  form,
-  type,
-  onClick,
-  ...others
-}) => {
+export const Button: FC<ButtonProps> = ({ form, type, onClick, ...others }) => {
   const handleClick = useCallback(
     (event: MouseEvent<ButtonDomRef>) => {
       if (onClick != null) {
