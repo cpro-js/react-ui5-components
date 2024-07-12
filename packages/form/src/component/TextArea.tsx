bc245708566ea5249c99997205159238d7539465
nwoellgens <nico.woellgens@cpro-conlog.com>
2024-07-12 08:54:43 +0200

    docs: filtered TextArea Props

--- packages/form/src/component/TextArea.tsx
+++ packages/form/src/component/TextArea.tsx
@@ -2,9 +2,49 @@ import "../form/formSupport";
 
 import { TextArea as UI5TextArea } from "@ui5/webcomponents-react";
 import { TextAreaDomRef, TextAreaPropTypes } from "@ui5/webcomponents-react";
-import { forwardRef } from "react";
+import { HTMLAttributes, forwardRef } from "react";
 
-export interface TextAreaProps extends TextAreaPropTypes {}
+// pick only those props which we do care about
+type SharedHtmlProps = Pick<
+  HTMLAttributes<HTMLElement>,
+  | "style"
+  | "className"
+  | "id"
+  | "placeholder"
+  | "title"
+  | "onKeyUp"
+  | "onKeyDown"
+  | "onBlur"
+  | "onFocus"
+  | "onPaste"
+  | "onMouseOver"
+  | "onMouseOut"
+  | "onMouseEnter"
+  | "onMouseLeave"
+  | "onMouseMove"
+>;
+
+export type TextAreaProps = SharedHtmlProps &
+  Pick<
+    TextAreaPropTypes,
+    | "valueStateMessage"
+    | "onChange"
+    | "onInput"
+    | "disabled"
+    | "growing"
+    | "growingMaxLines"
+    | "maxlength"
+    | "name"
+    | "placeholder"
+    | "readonly"
+    | "required"
+    | "rows"
+    | "showExceededText"
+    | "value"
+    | "valueState"
+  >;
+
+//export interface TextAreaProps extends TextAreaPropTypes {}
 
 /** `TextArea` as a wrapper of
  * <a href="https://sap.github.io/ui5-webcomponents-react/?path=/docs/inputs-textarea--docs" target="_blank">UI5 TextArea</a>
