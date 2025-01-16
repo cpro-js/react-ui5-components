41e9b5de84b1678c702c733a458146041043cfe5
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-01-16 12:56:30 +0100

    fix: importing construct-style-sheets-polyfill to solve ui5 webcomponents import bug

--- packages/form/.jest/setupTestsAfterEnv.js
+++ packages/form/.jest/setupTestsAfterEnv.js
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import "construct-style-sheets-polyfill";
 
 import { setProjectAnnotations } from "@storybook/react";
 import ResizeObserverPolyfill from "resize-observer-polyfill";
