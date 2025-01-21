b92c4068beb4a93ce4e10432a38fd48353d67329
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-01-21 12:33:07 +0100

    fix: refactor unit test function

--- packages/form/src/component/DatePicker.test.tsx
+++ packages/form/src/component/DatePicker.test.tsx
@@ -1,18 +1,20 @@
 import { convertToDate } from "./DatePicker";
 
 describe("convertToDate", () => {
+  const newDateFunction = (value: any) => new Date(value);
+
   test("should return null for null value", () => {
-    expect(convertToDate(null, (value) => new Date(value))).toBe(null);
+    expect(convertToDate(null, newDateFunction)).toBe(null);
   });
 
   test("should return the same Date object if value is a Date", () => {
     const date = new Date();
-    const result = convertToDate(date, (value) => new Date(value));
+    const result = convertToDate(date, newDateFunction);
     expect(result).toBe(date);
   });
 
   test("should parse a valid string date to a Date object", () => {
-    expect(convertToDate("2025-01-01", (value) => new Date(value))).toEqual(
+    expect(convertToDate("2025-01-01", newDateFunction)).toEqual(
       new Date("2025-01-01")
     );
   });
