71920e89516ac2c8972d6042a14484852b070d85
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-01-30 16:44:25 +0100

    Revert "chore: removed not working TextArea.stories.test file"
    This reverts commit 21f63529f248b34de88c83e71aabda6844704f22.

---
+++ packages/form/src/component/TextArea.stories.test.tsx
@@ -0,0 +1,14 @@
+import { StoryFn } from "@storybook/react";
+import { composeStories } from "@storybook/react";
+
+import { renderWithDefine } from "../test/render";
+import * as stories from "./TextArea.stories";
+
+const testCases = Object.values(composeStories(stories)).map(
+  (StoryFn: StoryFn) => [StoryFn.storyName!, StoryFn]
+);
+// Batch snapshot testing
+test.each(testCases)("Renders %s story", async (_storyName, StoryFn) => {
+  const tree = await renderWithDefine(<StoryFn />);
+  expect(tree.container).toMatchSnapshot();
+});
