e88d7b0c9cecdff9734261cdf63a9f3af659a862
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-01-30 13:43:06 +0100

    chore: externalize modul that failed to resolve by Rollup

--- packages/form/vite.config-build.ts
+++ packages/form/vite.config-build.ts
@@ -17,6 +17,9 @@ export default defineConfig({
       entry: resolve(__dirname, "src/index.ts"),
       formats: ["es", "cjs"],
     },
+    rollupOptions: {
+      external: ["storybook/internal/preview/runtime"],
+    },
   },
   plugins: [
     react({
