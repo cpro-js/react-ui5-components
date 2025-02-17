f805e98fef7f7f47ce3d906795b0795875300891
Jan-Andre Zinser <jan-andre.zinser@cpro-conlog.com>
2025-02-17 16:31:44 +0100

    refactor: form values types

--- packages/form/src/field/FormValues.tsx
+++ packages/form/src/field/FormValues.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from "react";
-import { useWatch } from "react-hook-form";
+import { FieldValues, useWatch } from "react-hook-form";
 
 import { PartialFormValues } from "./types";
 
-export type FormValuesProps<FormValues extends {}> = {
+export type FormValuesProps<FormValues extends FieldValues> = {
   render: (values: PartialFormValues<FormValues>) => ReactNode | null;
 };
 
-export function FormValues<T extends {}>(props: FormValuesProps<T>) {
+export function FormValues<T extends FieldValues>(props: FormValuesProps<T>) {
   const x = useWatch<T>({});
   return props.render(x as unknown as PartialFormValues<T>);
 }
