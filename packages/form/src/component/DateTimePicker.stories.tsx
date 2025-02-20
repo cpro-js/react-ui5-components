8cc54b75639915c71a528dc101851f94a38f4c39
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-02-20 18:03:30 +0100

    refactor: dateTimePicker stories refactored to CSF3.0

--- packages/form/src/component/DateTimePicker.stories.tsx
+++ packages/form/src/component/DateTimePicker.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from "@storybook/addon-actions";
-import { Meta, StoryFn } from "@storybook/react";
+import { Decorator, Meta, StoryFn, StoryObj } from "@storybook/react";
 
 import { ISODateTimeAdapter } from "../form/adapter/date/ISODateTimeAdapter";
 import { FormAdapter } from "../form/FormAdapter";
@@ -15,79 +15,94 @@ export default {
   },
 } satisfies Meta<typeof DateTimePicker>;
 
-const Template: StoryFn<typeof DateTimePicker> = (args) => {
-  return <DateTimePicker {...args} />;
-};
+type Story = StoryObj<typeof DateTimePicker>;
 
-export const Standard = Template.bind({});
-Standard.args = {
-  onChange: (...args) => {
-    console.log("onChange", ...args);
-    action("onChange")(...args);
-  },
-};
-export const CustomFormat = Template.bind({});
-CustomFormat.args = {
-  ...Standard.args,
-  formatPattern: "dd_MM_yyyy 'um' HH:mm",
-};
-
-export const Prefilled = Template.bind({});
-Prefilled.args = { ...Standard.args, value: new Date() };
-
-export const MinDateToday = Template.bind({});
-MinDateToday.args = { ...Standard.args, minDate: new Date() };
-
-export const MaxDateToday = Template.bind({});
-MaxDateToday.args = { ...Standard.args, maxDate: new Date() };
-
-const ISODateTimeTemplate: StoryFn<typeof DateTimePicker> = (args) => {
-  return (
-    <FormAdapter dateTime={ISODateTimeAdapter}>
-      <DateTimePicker {...args} />
-    </FormAdapter>
-  );
-};
-
-export const ISODateTimeStandard = ISODateTimeTemplate.bind({});
-ISODateTimeStandard.args = {
-  onChange: (...args) => {
-    console.log("onChange", ...args);
-    action("onChange")(...args);
+export const Standard: Story = {
+  args: {
+    onChange: (...args) => {
+      console.log("onChange", ...args);
+      action("onChange")(...args);
+    },
   },
 };
 
-// TODO iso date string does not for type text: https://github.com/storybookjs/storybook/issues/13713
-ISODateTimeStandard.argTypes = {
-  value: { type: "string", control: "text" },
-  minDate: { type: "string", control: "text" },
-  maxDate: { type: "string", control: "text" },
+export const CustomFormat: Story = {
+  args: {
+    ...Standard.args,
+    formatPattern: "dd_MM_yyyy 'um' HH:mm",
+  },
 };
 
-export const ISODateTimeCustomFormat = ISODateTimeTemplate.bind({});
-ISODateTimeCustomFormat.args = {
-  ...ISODateTimeStandard.args,
-  formatPattern: "dd_MM_yyyy 'ab' HH:mm:ss",
+export const Prefilled: Story = {
+  args: { ...Standard.args, value: new Date() },
 };
-ISODateTimeCustomFormat.argTypes = { ...ISODateTimeStandard.argTypes };
 
-export const ISODateTimePrefilled = ISODateTimeTemplate.bind({});
-ISODateTimePrefilled.args = {
-  ...ISODateTimeStandard.args,
-  value: new Date().toISOString(),
+export const MinDateToday: Story = {
+  args: { ...Standard.args, minDate: new Date() },
 };
-ISODateTimePrefilled.argTypes = { ...ISODateTimeStandard.argTypes };
 
-export const ISODateTimeMinDateToday = ISODateTimeTemplate.bind({});
-ISODateTimeMinDateToday.args = {
-  ...ISODateTimeStandard.args,
-  minDate: new Date().toISOString(),
+export const MaxDateToday: Story = {
+  args: { ...Standard.args, maxDate: new Date() },
 };
-ISODateTimeMinDateToday.argTypes = { ...ISODateTimeStandard.argTypes };
 
-export const ISODateTimeMaxDateToday = ISODateTimeTemplate.bind({});
-ISODateTimeMaxDateToday.args = {
-  ...ISODateTimeStandard.args,
-  maxDate: new Date().toISOString(),
+const ISODateTimeDecorator: Decorator = (Story) => (
+  <FormAdapter dateTime={ISODateTimeAdapter}>
+    <Story />
+  </FormAdapter>
+);
+
+export const ISODateTimeStandard: Story = {
+  args: {
+    onChange: (...args) => {
+      console.log("onChange", ...args);
+      action("onChange")(...args);
+    },
+  },
+
+  // TODO iso date string does not for type text: https://github.com/storybookjs/storybook/issues/13713
+  argTypes: {
+    value: { type: "string", control: "text" },
+    minDate: { type: "string", control: "text" },
+    maxDate: { type: "string", control: "text" },
+  },
+
+  decorators: [ISODateTimeDecorator],
 };
-ISODateTimeMaxDateToday.argTypes = { ...ISODateTimeStandard.argTypes };
+
+export const ISODateTimeCustomFormat: Story = {
+  ...ISODateTimeStandard,
+  args: {
+    ...ISODateTimeStandard.args,
+    formatPattern: "dd_MM_yyyy 'ab' HH:mm:ss",
+  },
+
+  decorators: [ISODateTimeDecorator],
+};
+
+export const ISODateTimePrefilled: Story = {
+  ...ISODateTimeStandard,
+  args: {
+    ...ISODateTimeStandard.args,
+    value: new Date().toISOString(),
+  },
+
+  decorators: [ISODateTimeDecorator],
+};
+
+export const ISODateTimeMinDateToday: Story = {
+  ...ISODateTimeStandard,
+  args: {
+    ...ISODateTimeStandard.args,
+    minDate: new Date().toISOString(),
+  },
+  decorators: [ISODateTimeDecorator],
+};
+
+export const ISODateTimeMaxDateToday: Story = {
+  ...ISODateTimeStandard,
+  args: {
+    ...ISODateTimeStandard.args,
+    maxDate: new Date().toISOString(),
+  },
+  decorators: [ISODateTimeDecorator],
+};
