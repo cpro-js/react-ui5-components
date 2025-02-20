30195ea596b80dd2ac493e6f47f5e884285d2a02
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-02-20 18:03:52 +0100

    refactor: multiAutoComplete stories refactored to CSF3.0

--- packages/form/src/component/MultiAutoComplete.stories.tsx
+++ packages/form/src/component/MultiAutoComplete.stories.tsx
@@ -1,7 +1,7 @@
 import "@ui5/webcomponents-icons/dist/add.js";
 
 import { action } from "@storybook/addon-actions";
-import { Meta, StoryFn } from "@storybook/react";
+import { Decorator, Meta, StoryFn, StoryObj } from "@storybook/react";
 
 import {
   COUNTRIES,
@@ -15,7 +15,9 @@ export default {
   component: MultiAutoComplete,
 } satisfies Meta<typeof MultiAutoComplete>;
 
-const Template: StoryFn<typeof MultiAutoComplete<DefaultAutoCompletOption>> = (
+type Story = StoryObj<typeof MultiAutoComplete<DefaultAutoCompletOption>>;
+
+/*const Template: StoryFn<typeof MultiAutoComplete<DefaultAutoCompletOption>> = (
   args
 ) => {
   const onSelect = (values: Array<string>) => {
@@ -30,46 +32,70 @@ const Template: StoryFn<typeof MultiAutoComplete<DefaultAutoCompletOption>> = (
       style={{ width: "50%" }}
     />
   );
+};*/
+
+const MultiAutoCompleteDecorator: Decorator = (Story) => {
+  const onSelect = (values: Array<string>) => {
+    // setStoredValues(values);
+    action("onSelect")(values);
+  };
+
+  return <Story onSelectionChange={onSelect} style={{ width: "50%" }} />;
 };
 
-export const Standard = Template.bind({});
-Standard.args = { values: [], onSearch: SEARCH_COUNTRIES };
-
-export const Prefilled = Template.bind({});
-Prefilled.args = {
-  ...Standard.args,
-  values: [COUNTRIES[0].value, COUNTRIES[4].value, COUNTRIES[5].value],
-  initialSuggestions: [COUNTRIES[0], COUNTRIES[4], COUNTRIES[5]],
+export const Standard: Story = {
+  args: { values: [], onSearch: SEARCH_COUNTRIES },
+  decorators: [MultiAutoCompleteDecorator],
 };
 
-export const CustomLabelProp = Template.bind({});
-CustomLabelProp.args = { ...Prefilled.args, itemLabel: "withUmlaut" };
-
-export const CustomLabelFunction = Template.bind({});
-CustomLabelFunction.args = {
-  ...Prefilled.args,
-  itemLabel: (country) => `ISO code: ${country.value}`,
-};
-
-export const CustomValueProp = Template.bind({});
-CustomValueProp.args = { ...Prefilled.args, itemValue: "label" };
-
-export const CustomValueFunction = Template.bind({});
-CustomValueFunction.args = {
-  ...Prefilled.args,
-  itemValue: (country) => `VALUE_${country.label.toUpperCase()}`,
-};
-
-export const RenderValue = Template.bind({});
-RenderValue.args = {
-  ...Prefilled.args,
-  renderValue: (country) => {
-    return {
-      text: country.label + " - custom !!!",
-      // value: `VALUE_${country.label.toUpperCase()}`,
-      // closeIcon: <Icon name="refresh" />,
-      className: "test-class-name",
-      // selected: true,
-    };
+export const Prefilled: Story = {
+  args: {
+    ...Standard.args,
+    values: [COUNTRIES[0].value, COUNTRIES[4].value, COUNTRIES[5].value],
+    initialSuggestions: [COUNTRIES[0], COUNTRIES[4], COUNTRIES[5]],
   },
+
+  decorators: [MultiAutoCompleteDecorator],
+};
+
+export const CustomLabelProp: Story = {
+  args: { ...Prefilled.args, itemLabel: "withUmlaut" },
+  decorators: [MultiAutoCompleteDecorator],
+};
+
+export const CustomLabelFunction: Story = {
+  args: {
+    ...Prefilled.args,
+    itemLabel: (country) => `ISO code: ${country.value}`,
+  },
+  decorators: [MultiAutoCompleteDecorator],
+};
+
+export const CustomValueProp: Story = {
+  args: { ...Prefilled.args, itemValue: "label" },
+  decorators: [MultiAutoCompleteDecorator],
+};
+
+export const CustomValueFunction: Story = {
+  args: {
+    ...Prefilled.args,
+    itemValue: (country) => `VALUE_${country.label.toUpperCase()}`,
+  },
+  decorators: [MultiAutoCompleteDecorator],
+};
+
+export const RenderValue: Story = {
+  args: {
+    ...Prefilled.args,
+    renderValue: (country) => {
+      return {
+        text: country.label + " - custom !!!",
+        // value: `VALUE_${country.label.toUpperCase()}`,
+        // closeIcon: <Icon name="refresh" />,
+        className: "test-class-name",
+        // selected: true,
+      };
+    },
+  },
+  decorators: [MultiAutoCompleteDecorator],
 };
