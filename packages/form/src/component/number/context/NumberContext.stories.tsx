e99cc6455f5306339d56c646935fb8b68bf7d045
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-04-01 15:47:18 +0200

    Revert "refactor: numberContextProvider"
    This reverts commit efb226e269023c3f442a2aa0a20a84aad0aaa00a.

--- packages/form/src/component/number/context/NumberContext.stories.tsx
+++ packages/form/src/component/number/context/NumberContext.stories.tsx
@@ -1,39 +1,13 @@
-import { Decorator, Meta, StoryFn, StoryObj } from "@storybook/react";
+import { Meta, StoryFn } from "@storybook/react";
 
 import { CurrencyInput } from "../CurrencyInput";
 import { NumberInput } from "../NumberInput";
 import { NumberContextProvider } from "./NumberContext";
 
 export default {
-  title: "Component/NumberContextProvider",
+  title: "Form/Component/NumberContextProvider",
   component: NumberContextProvider,
 } satisfies Meta<typeof NumberContextProvider>;
-
-type Story = StoryObj<typeof NumberContextProvider>;
-
-const NumberDecorator: Decorator = (Story, context) => {
-  const { args } = context;
-  const value = 1000555.482;
-
-  return (
-    <>
-      <h2>Locale Provider with Locale [{args.locale as string}]</h2>
-      <NumberContextProvider {...args}>
-        <h3>Number Input via Provider</h3>
-        <NumberInput value={value} />
-        <h3>Currency Input via Provider</h3>
-        <CurrencyInput value={value} />
-        <h3>Explicit IN</h3>
-        <NumberInput value={value} locale="en-IN" />
-        <CurrencyInput value={value} locale="en-IN" currency="INR" />
-        <h3>Explicit JA (without fraction digits for currency)</h3>
-        <NumberInput value={value} locale="ja-JP" />
-        <CurrencyInput value={value} locale="ja-JP" currency="JPY" />
-        <Story />
-      </NumberContextProvider>
-    </>
-  );
-};
 
 const Template: StoryFn<typeof NumberContextProvider> = (args) => {
   const value = 1000555.482;
@@ -57,51 +31,45 @@ const Template: StoryFn<typeof NumberContextProvider> = (args) => {
   );
 };
 
-export const Standard = {
-  args: { locale: "en", currency: "USD", useGrouping: true },
-  decorators: [NumberDecorator],
-} satisfies Story;
+export const Standard = Template.bind({});
+Standard.args = { locale: "en", currency: "USD", useGrouping: true };
 
-export const Locale_DE = {
-  args: { locale: "de", currency: "EUR", useGrouping: true },
-  decorators: [NumberDecorator],
-} satisfies Story;
+export const Locale_DE = Template.bind({});
+Locale_DE.args = { locale: "de", currency: "EUR", useGrouping: true };
 
-export const Nested = {
-  render: () => {
-    const value = 1000555.482;
+export const Nested: StoryFn<typeof NumberContextProvider> = (args) => {
+  const value = 1000555.482;
 
-    return (
-      <NumberContextProvider locale="en-IN" currency="USD" useGrouping>
-        <h2>Outer Part (en-IN)</h2>
-        <NumberInput value={value} />
-        <CurrencyInput value={value} currency="INR" />
+  return (
+    <NumberContextProvider locale="en-IN" currency="USD" useGrouping>
+      <h2>Outer Part (en-IN)</h2>
+      <NumberInput value={value} />
+      <CurrencyInput value={value} currency="INR" />
 
-        <div>
-          <h2>Inner Part (de)</h2>
-          <NumberContextProvider locale="de" currency="EUR">
-            <NumberInput value={value} />
-            <CurrencyInput value={value} />
-            <CurrencyInput value={value} showCurrency={false} />
-          </NumberContextProvider>
-        </div>
+      <div>
+        <h2>Inner Part (de)</h2>
+        <NumberContextProvider locale="de" currency="EUR">
+          <NumberInput value={value} />
+          <CurrencyInput value={value} />
+          <CurrencyInput value={value} showCurrency={false} />
+        </NumberContextProvider>
+      </div>
 
-        <div>
-          <h2>No Grouping, special warning message, 1 maxFractionDigits</h2>
-          <NumberContextProvider
-            useGrouping={false}
-            maximumFractionDigits={1}
-            showCurrency={false}
-            getNumberWarningMessage={(type, discardedValue) =>
-              `${type}: ${discardedValue}`
-            }
-          >
-            <NumberInput value={value} />
-            <CurrencyInput value={value} />
-            <CurrencyInput value={value} showCurrency />
-          </NumberContextProvider>
-        </div>
-      </NumberContextProvider>
-    );
-  },
-} satisfies Story;
+      <div>
+        <h2>No Grouping, special warning message, 1 maxFractionDigits</h2>
+        <NumberContextProvider
+          useGrouping={false}
+          maximumFractionDigits={1}
+          showCurrency={false}
+          getNumberWarningMessage={(type, discardedValue) =>
+            `${type}: ${discardedValue}`
+          }
+        >
+          <NumberInput value={value} />
+          <CurrencyInput value={value} />
+          <CurrencyInput value={value} showCurrency />
+        </NumberContextProvider>
+      </div>
+    </NumberContextProvider>
+  );
+};
