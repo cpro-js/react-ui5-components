960ecbbaa944c23ef22f2c426264096867910cd0
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-04-09 17:32:51 +0200

    refactor: checkboxField.stories CSF 3.0

--- packages/form/src/field/CheckboxField.stories.tsx
+++ packages/form/src/field/CheckboxField.stories.tsx
@@ -1,8 +1,8 @@
-import { StoryFn } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { useRef } from "react";
 
-import { FormController, FormControllerProps } from "../form/FormController";
-import { CheckboxField, CheckboxFieldProps } from "./CheckboxField";
+import { FormController } from "../form/FormController";
+import { CheckboxField } from "./CheckboxField";
 import { FormViewer, useFormViewer } from "./FormViewer";
 import { FormFieldRef } from "./types";
 
@@ -14,95 +14,125 @@ interface FormDataBoolean {
   value?: boolean;
 }
 
-const createTemplate = function <T extends {}>(): StoryFn<
-  FormControllerProps<T> & CheckboxFieldProps<FormData, "value">
-> {
-  return (args) => {
-    const { initialValues, onSubmit, ...props } = args;
-
-    const { submittedValues, handleSubmit } = useFormViewer<T>({
-      onSubmit: onSubmit,
-    });
-    const fieldRef = useRef<FormFieldRef<FormData, "value">>(null);
-
-    return (
-      <FormController<T> {...{ initialValues, onSubmit: handleSubmit }}>
-        <CheckboxField {...props} ref={fieldRef} name={"value"} />
-        <FormViewer submittedValues={submittedValues} fieldRef={fieldRef} />
-      </FormController>
-    );
-  };
-};
-
-const Template = createTemplate<FormData>();
-
-export const Empty = Template.bind({});
-Empty.args = {};
-
-export const Prefilled = Template.bind({});
-Prefilled.args = {
-  ...Empty.args,
-  initialValues: {
-    value: "on",
-  },
-};
-
-export const Disabled = Template.bind({});
-Disabled.args = {
-  ...Prefilled.args,
-  disabled: true,
-};
-
-export const Readonly = Template.bind({});
-Readonly.args = {
-  ...Prefilled.args,
-  readonly: true,
-};
-
-export const ValidationRequired = Template.bind({});
-ValidationRequired.args = {
-  required: true,
-};
-
-const TemplateBoolean = createTemplate<FormDataBoolean>();
-
-export const BooleanEmpty = TemplateBoolean.bind({});
-BooleanEmpty.args = {
-  boolean: true,
-};
-
-export const BooleanPrefilled = TemplateBoolean.bind({});
-BooleanPrefilled.args = {
-  ...BooleanEmpty.args,
-  initialValues: {
-    value: true,
-  },
-};
-
-export const BooleanDisabled = TemplateBoolean.bind({});
-BooleanDisabled.args = {
-  ...BooleanPrefilled.args,
-  disabled: true,
-};
-
-export const BooleanReadonly = TemplateBoolean.bind({});
-BooleanReadonly.args = {
-  ...BooleanPrefilled.args,
-  readonly: true,
-};
-
-export const BooleanValidationRequired = TemplateBoolean.bind({});
-BooleanValidationRequired.args = {
-  ...BooleanEmpty.args,
-  required: true,
-};
-
 export default {
   title: "Form/Field/CheckboxField",
   component: CheckboxField,
-  argTypes: {
-    onSubmit: {
-      action: "submit",
+  parameters: {
+    form: {
+      initialValues: {},
     },
   },
+  render(props, context) {
+    const { onSubmit, initialValues, ...formControllerProps } =
+      context.parameters.form;
+    const fieldRef = useRef<FormFieldRef<FormData, "value">>(null);
+
+    const isBoolean = typeof initialValues?.value === "boolean";
+
+    if (isBoolean) {
+      const { submittedValues, handleSubmit } = useFormViewer<FormDataBoolean>({
+        onSubmit,
+      });
+
+      return (
+        <FormController<FormDataBoolean>
+          {...formControllerProps}
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+        >
+          <CheckboxField {...props} ref={fieldRef} name="value" />
+          <FormViewer submittedValues={submittedValues} fieldRef={fieldRef} />
+        </FormController>
+      );
+    } else {
+      const { submittedValues, handleSubmit } = useFormViewer<FormData>({
+        onSubmit,
+      });
+
+      return (
+        <FormController<FormData>
+          {...formControllerProps}
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+        >
+          <CheckboxField {...props} ref={fieldRef} name="value" />
+          <FormViewer submittedValues={submittedValues} fieldRef={fieldRef} />
+        </FormController>
+      );
+    }
+  },
+} satisfies Meta<typeof CheckboxField>;
+
+type Story = StoryObj<typeof CheckboxField>;
+
+export const Empty = {
+  args: {},
+} satisfies Story;
+
+export const Prefilled = {
+  parameters: {
+    form: {
+      initialValues: {
+        value: "on",
+      },
+    },
+  },
+} satisfies Story;
+
+export const Disabled = {
+  args: {
+    disabled: true,
+  },
+} satisfies Story;
+
+export const Readonly = {
+  args: {
+    readonly: true,
+  },
+} satisfies Story;
+
+export const ValidationRequired = {
+  args: {
+    required: true,
+  },
+} satisfies Story;
+
+export const BooleanEmpty = {
+  args: {
+    boolean: true,
+  },
 };
+
+export const BooleanPrefilled = {
+  args: {
+    ...BooleanEmpty.args,
+  },
+  parameters: {
+    form: {
+      initialValues: {
+        value: true,
+      },
+    },
+  },
+} satisfies Story;
+
+export const BooleanDisabled = {
+  args: {
+    ...BooleanEmpty.args,
+    disabled: true,
+  },
+} satisfies Story;
+
+export const BooleanReadonly = {
+  args: {
+    ...BooleanEmpty.args,
+    readonly: true,
+  },
+} satisfies Story;
+
+export const BooleanValidationRequired = {
+  args: {
+    ...BooleanEmpty.args,
+    required: true,
+  },
+} satisfies Story;
