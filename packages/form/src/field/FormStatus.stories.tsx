903facf08c579a6a9b238c44d6108404510717fa
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-04-11 16:33:20 +0200

    refactor: formStatus to CSF3.0

--- packages/form/src/field/FormStatus.stories.tsx
+++ packages/form/src/field/FormStatus.stories.tsx
@@ -1,7 +1,8 @@
 import { action } from "@storybook/addon-actions";
-import { StoryFn } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
+import type { SubmitHandler } from "react-hook-form";
 
-import { FormController, FormControllerProps } from "../form/FormController";
+import { FormController } from "../form/FormController";
 import { FormStatus, FormStatusProps } from "./FormStatus";
 import { TextInputField } from "./TextInputField";
 
@@ -9,39 +10,49 @@ interface FormData {
   text?: string;
 }
 
-const Template: StoryFn<FormControllerProps<FormData> & FormStatusProps> = (
-  args
-) => {
-  const { initialValues, onSubmit } = args;
-
-  return (
-    <FormController<FormData> {...{ initialValues, onSubmit }}>
-      <TextInputField name={"text"} />
-      <div>
-        <button type="submit">Submit</button>
-        <button type="reset">Reset</button>
-      </div>
-      <FormStatus {...args} />
-    </FormController>
-  );
-};
-
-export const Standard = Template.bind({});
-Standard.args = {
-  onSubmit: async (...args) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    action("submit")(...args);
-  },
-  render: (state) => (
-    <div>
-      Submitting {JSON.stringify(state.isSubmitting)}, Dirty{" "}
-      {JSON.stringify(state.isDirty)}, Valid {JSON.stringify(state.isValid)},
-      Validating {JSON.stringify(state.isValidating)}
-    </div>
-  ),
-};
-
-export default {
+const meta = {
   title: "Form/Field/FormStatus",
   component: FormStatus,
-};
+  parameters: {
+    form: {
+      initialValues: {},
+      onSubmit: (async (...args) => {
+        await new Promise((r) => setTimeout(r, 2000));
+        action("submit")(...args);
+      }) satisfies SubmitHandler<FormData>,
+    },
+  },
+  render(args, context) {
+    const { initialValues, onSubmit } = context.parameters.form;
+
+    return (
+      <FormController<FormData>
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+      >
+        <TextInputField name="text" />
+        <div>
+          <button type="submit">Submit</button>
+          <button type="reset">Reset</button>
+        </div>
+        <FormStatus {...args} />
+      </FormController>
+    );
+  },
+} satisfies Meta<typeof FormStatus>;
+
+export default meta;
+
+type Story = StoryObj<typeof FormStatus>;
+
+export const Standard = {
+  args: {
+    render: (state) => (
+      <div>
+        Submitting {JSON.stringify(state.isSubmitting)}, Dirty{" "}
+        {JSON.stringify(state.isDirty)}, Valid {JSON.stringify(state.isValid)},
+        Validating {JSON.stringify(state.isValidating)}
+      </div>
+    ),
+  },
+} satisfies Story;
