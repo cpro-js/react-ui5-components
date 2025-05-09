972ed07aa35175261a8f8a6a9a15a8278c412b35
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-05-09 11:23:20 +0200

    feat: timePickerField & stories

---
+++ packages/form/src/field/TImePickerField.stories.tsx
@@ -0,0 +1,81 @@
+import { action } from "@storybook/addon-actions";
+import { Meta, StoryObj } from "@storybook/react";
+import { useRef } from "react";
+
+import { FormController } from "../form/FormController";
+import { FormViewer, useFormViewer } from "./FormViewer";
+import { TimePickerField } from "./TimePickerFIeld";
+import { FormFieldRef } from "./types";
+
+interface FormData {
+  time?: string;
+}
+
+const meta = {
+  title: "Form/Field/TimePickerField",
+  component: TimePickerField,
+  parameters: {
+    form: {
+      initialValues: {},
+      onSubmit: action("form-submit"),
+    },
+  },
+  render(props, context) {
+    const { initialValues, onSubmit, ...formControllerProps } =
+      context.parameters.form;
+    const { submittedValues, handleSubmit } = useFormViewer<FormData>({
+      onSubmit: onSubmit,
+    });
+    const fieldRef = useRef<FormFieldRef<FormData, "time">>(null);
+
+    return (
+      <FormController
+        {...formControllerProps}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        <TimePickerField {...props} ref={fieldRef} name={"time"} />
+        <FormViewer submittedValues={submittedValues} fieldRef={fieldRef} />
+      </FormController>
+    );
+  },
+} satisfies Meta<typeof TimePickerField>;
+
+export default meta;
+
+type Story = StoryObj<typeof TimePickerField>;
+
+export const Empty = {
+  args: {},
+} satisfies Story;
+
+export const Prefilled = {
+  args: {
+    formatPattern: "HH:mm",
+  },
+  parameters: {
+    form: {
+      initialValues: {
+        time: "12:55",
+      },
+    },
+  },
+} satisfies Story;
+
+export const Disabled = {
+  args: {
+    disabled: true,
+  },
+} satisfies Story;
+
+export const Readonly = {
+  args: {
+    readonly: true,
+  },
+} satisfies Story;
+
+export const ValidationRequired = {
+  args: {
+    required: true,
+  },
+} satisfies Story;
---
+++ packages/form/src/field/TimePickerFIeld.tsx
@@ -0,0 +1,165 @@
+import { TimePickerDomRef } from "@ui5/webcomponents-react";
+import {
+  ReactElement,
+  Ref,
+  forwardRef,
+  useContext,
+  useImperativeHandle,
+  useRef,
+} from "react";
+import {
+  FieldError,
+  FieldPath,
+  FieldPathValue,
+  FieldValues,
+} from "react-hook-form";
+import { useEventCallback } from "usehooks-ts";
+
+import { TimePicker, TimePickerProps } from "../component/TimePicker";
+import { useControlledField } from "../form/_internal/useField";
+import { FormAdapterContext } from "../form/FormAdapter";
+import { useCustomEventDispatcher } from "../hook/useCustomEventDispatcher";
+import {
+  FieldEventDetail,
+  FormFieldChangeEvent,
+  FormFieldCommonProps,
+  FormFieldRef,
+  FormFieldSubmitEvent,
+  FormFieldValidation,
+} from "./types";
+
+const isErrorIgnored = (error: FieldError | undefined) =>
+  error != null && error.type === "pattern";
+
+export type TimePickerFieldProps<
+  FormValues extends FieldValues,
+  FormFieldName extends FieldPath<FormValues>
+> = Omit<
+  TimePickerProps,
+  | "name"
+  | "value"
+  | "valueState"
+  | "valueStateMessage"
+  | "onChange"
+  | "onSubmit"
+> &
+  Pick<
+    FormFieldValidation<FormValues, FieldPathValue<FormValues, FormFieldName>>,
+    "required" | "validate"
+  > &
+  FormFieldCommonProps<FormValues, FormFieldName> & {
+    onChange?: (
+      event: FormFieldChangeEvent<TimePickerDomRef, FormValues, FormFieldName>
+    ) => void;
+    onSubmit?: (
+      event: FormFieldSubmitEvent<TimePickerDomRef, FormValues, FormFieldName>
+    ) => void;
+  };
+
+export const TimePickerField = forwardRef<
+  FormFieldRef<any, any>,
+  TimePickerFieldProps<any, any>
+>((props, forwardedRef) => {
+  const {
+    name,
+    required,
+    validate,
+    dependsOn,
+    onChange,
+    onSubmit,
+    onBlur,
+    ...timePickerProps
+  } = props;
+
+  const elementRef = useRef<TimePickerDomRef>(null);
+
+  const field = useControlledField({
+    ref: elementRef,
+    name,
+    required,
+    validate,
+    dependsOn,
+  });
+
+  useImperativeHandle(forwardedRef, () => field.fieldApiRef.current, [
+    field.fieldApiRef,
+  ]);
+
+  useImperativeHandle(field.ref, () => elementRef.current, []);
+
+  const dispatchChangeEvent = useCustomEventDispatcher<
+    TimePickerDomRef,
+    FieldEventDetail<any, any>
+  >({
+    ref: elementRef,
+    name: "field-change",
+    onEvent: onChange,
+  });
+
+  const dispatchSubmitEvent = useCustomEventDispatcher<
+    TimePickerDomRef,
+    FieldEventDetail<any, any>
+  >({
+    ref: elementRef,
+    name: "field-submit",
+    onEvent: onSubmit,
+  });
+
+  return (
+    <TimePicker
+      {...timePickerProps}
+      ref={elementRef}
+      name={field.name}
+      value={field.value === undefined ? null : field.value}
+      readonly={props.readonly || field.isValidating || field.isSubmitting}
+      required={required}
+      valueState={field.valueState}
+      valueStateMessage={
+        !isErrorIgnored(field.error) &&
+        field.valueStateMessage != null && (
+          <div slot="valueStateMessage">{field.valueStateMessage}</div>
+        )
+      }
+      onChange={useEventCallback(async (event) => {
+        event.stopPropagation();
+
+        const value = event.detail.value;
+        field.fieldApiRef.current.setValue(value);
+        const valid = await field.fieldApiRef.current.validate();
+
+        dispatchChangeEvent({
+          name,
+          value,
+          valid,
+          field: field.fieldApiRef.current,
+          form: field.formApiRef.current,
+        });
+      })}
+      onSubmit={useEventCallback(async (event) => {
+        event.stopPropagation();
+
+        const value = field.fieldApiRef.current.getValue();
+        const valid = await field.fieldApiRef.current.validate();
+
+        dispatchSubmitEvent({
+          name,
+          value,
+          valid,
+          field: field.fieldApiRef.current,
+          form: field.formApiRef.current,
+        });
+      })}
+      onBlur={useEventCallback((event) => {
+        onBlur?.(event);
+        field.onBlur();
+      })}
+    />
+  );
+}) as <
+  FormValues extends FieldValues,
+  FormFieldName extends FieldPath<FormValues>
+>(
+  p: TimePickerFieldProps<FormValues, FormFieldName> & {
+    ref?: Ref<FormFieldRef<FormValues, FormFieldName>>;
+  }
+) => ReactElement;
