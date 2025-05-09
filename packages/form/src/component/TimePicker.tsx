7bd931dcb8dd0bdd1104497cc2d32c073640cd64
N1co2001 <124589988+N1co2001@users.noreply.github.com>
2025-05-09 13:29:27 +0200

    feat: timePickerComponent (#127)

---
+++ packages/form/src/component/TimePicker.tsx
@@ -0,0 +1,179 @@
+import {
+  TimePickerDomRef,
+  TimePickerPropTypes,
+  TimePicker as UI5TimePicker,
+} from "@ui5/webcomponents-react";
+import {
+  FocusEvent,
+  KeyboardEvent,
+  MutableRefObject,
+  ReactElement,
+  Ref,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { useEventCallback } from "usehooks-ts";
+
+import {
+  TypedCustomEvent,
+  useCustomEventDispatcher,
+} from "../hook/useCustomEventDispatcher";
+import { GlobalHtmlKeyInputElementProps } from "./GlobalHtmlElementProps";
+import { useFireSubmit } from "./util";
+
+export const durationToTime = (
+  duration: string,
+  formatPattern: string
+): string => {
+  const parts = duration.split(":");
+  const length = formatPattern.split(":").length;
+  return parts.slice(0, length).join(":");
+};
+
+export const normalizeTimeValue = (
+  timeInput: string,
+  formatPattern: string
+): string => {
+  const parts = timeInput.split(":");
+
+  while (parts.length < 3) {
+    parts.push("00");
+  }
+
+  return parts.join(":");
+};
+
+export type TimePickerProps = GlobalHtmlKeyInputElementProps<TimePickerDomRef> &
+  Pick<
+    TimePickerPropTypes,
+    | "disabled"
+    | "formatPattern"
+    | "name"
+    | "placeholder"
+    | "required"
+    | "readonly"
+    | "valueState"
+    | "valueStateMessage"
+  > & {
+    formatPattern?: "HH:mm" | "HH:mm:ss";
+    value?: string; // expected: "HH:mm:ss"
+    onChange?: (
+      event: TypedCustomEvent<TimePickerDomRef, { value: string | undefined }>
+    ) => void;
+    onSubmit?: (
+      event: TypedCustomEvent<TimePickerDomRef, { value: string | undefined }>
+    ) => void;
+  };
+
+export const TimePicker = forwardRef<TimePickerDomRef | null, TimePickerProps>(
+  (props, forwardedRef) => {
+    const {
+      className,
+      value,
+      onFocus,
+      onKeyDown,
+      onKeyUp,
+      onChange,
+      onSubmit,
+      formatPattern = "HH:mm",
+      ...passThroughProps
+    } = props;
+
+    const ref = useRef<TimePickerDomRef>(
+      null
+    ) as MutableRefObject<TimePickerDomRef | null>;
+
+    useImperativeHandle<TimePickerDomRef | null, TimePickerDomRef | null>(
+      forwardedRef,
+      () => ref.current,
+      []
+    );
+
+    const [isRefSet, setIsRefSet] = useState(false);
+
+    const setRef = useCallback((ui5TimePicker: TimePickerDomRef | null) => {
+      ref.current = ui5TimePicker;
+      setIsRefSet(true);
+    }, []);
+
+    const submit = useFireSubmit();
+
+    const dispatchChangeEvent = useCustomEventDispatcher<
+      TimePickerDomRef,
+      { value: string | undefined }
+    >({
+      ref,
+      name: "cpro-change",
+      onEvent: onChange,
+    });
+
+    const dispatchSubmitEvent = useCustomEventDispatcher<
+      TimePickerDomRef,
+      { value: string | undefined }
+    >({
+      ref,
+      name: "cpro-submit",
+      onEvent: onSubmit,
+      delay: 0,
+    });
+
+    const lastValue = useRef<string | undefined>(value);
+
+    useEffect(() => {
+      if (value) {
+        lastValue.current = normalizeTimeValue(
+          durationToTime(value, formatPattern),
+          formatPattern
+        );
+      }
+    }, [value, formatPattern]);
+
+    const finalValue = value ? durationToTime(value, formatPattern) : "";
+
+    return (
+      <UI5TimePicker
+        {...passThroughProps}
+        ref={setRef}
+        className={className}
+        formatPattern={formatPattern}
+        value={finalValue}
+        onFocus={useEventCallback((e) => {
+          submit.focus();
+          onFocus?.(e as FocusEvent<TimePickerDomRef>);
+        })}
+        onKeyDown={useEventCallback((e) => {
+          submit.keyDown(e);
+          onKeyDown?.(e as KeyboardEvent<TimePickerDomRef>);
+        })}
+        onKeyUp={useEventCallback((event) => {
+          onKeyUp?.(event as KeyboardEvent<TimePickerDomRef>);
+          if (submit.shouldFireSubmitOnKeyUp()) {
+            dispatchSubmitEvent({ value: lastValue.current });
+          }
+        })}
+        onChange={useEventCallback((event) => {
+          event.stopPropagation();
+
+          const rawTime = event.detail.value;
+          const valid = event.detail.valid;
+          const formatted = valid
+            ? normalizeTimeValue(rawTime, formatPattern)
+            : undefined;
+          lastValue.current = formatted;
+          dispatchChangeEvent({ value: formatted });
+
+          if (submit.shouldFireSubmitOnChange()) {
+            dispatchSubmitEvent({ value: formatted });
+          }
+        })}
+      />
+    );
+  }
+) as (
+  p: TimePickerProps & { ref?: Ref<TimePickerDomRef | undefined> }
+) => ReactElement;
---
+++ packages/form/src/field/TimePickerField.tsx
@@ -0,0 +1,188 @@
+import { TimePickerDomRef } from "@ui5/webcomponents-react";
+import {
+  ReactElement,
+  Ref,
+  forwardRef,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
+import {
+  FieldError,
+  FieldPath,
+  FieldPathValue,
+  FieldValues,
+} from "react-hook-form";
+import { useEventCallback } from "usehooks-ts";
+
+import {
+  TimePicker,
+  TimePickerProps,
+  durationToTime,
+  normalizeTimeValue,
+} from "../component/TimePicker";
+import { useControlledField } from "../form/_internal/useField";
+import { useCustomEventDispatcher } from "../hook/useCustomEventDispatcher";
+import {
+  FieldEventDetail,
+  FormFieldChangeEvent,
+  FormFieldCommonProps,
+  FormFieldRef,
+  FormFieldSubmitEvent,
+  FormFieldValidation,
+} from "./types";
+
+const isErrorIgnored = (error: FieldError | undefined) =>
+  error != null && error.type === "pattern";
+
+export type TimePickerFieldProps<
+  FormValues extends FieldValues,
+  FormFieldName extends FieldPath<FormValues>
+> = Omit<
+  TimePickerProps,
+  | "name"
+  | "value"
+  | "valueState"
+  | "valueStateMessage"
+  | "onChange"
+  | "onSubmit"
+> &
+  Pick<
+    FormFieldValidation<FormValues, FieldPathValue<FormValues, FormFieldName>>,
+    "required" | "validate"
+  > &
+  FormFieldCommonProps<FormValues, FormFieldName> & {
+    onChange?: (
+      event: FormFieldChangeEvent<TimePickerDomRef, FormValues, FormFieldName>
+    ) => void;
+    onSubmit?: (
+      event: FormFieldSubmitEvent<TimePickerDomRef, FormValues, FormFieldName>
+    ) => void;
+  };
+
+export const TimePickerField = forwardRef<
+  FormFieldRef<any, any>,
+  TimePickerFieldProps<any, any>
+>((props, forwardedRef) => {
+  const {
+    name,
+    required,
+    validate,
+    dependsOn,
+    onChange,
+    onSubmit,
+    onBlur,
+    ...timePickerProps
+  } = props;
+
+  const elementRef = useRef<TimePickerDomRef>(null);
+
+  const field = useControlledField({
+    ref: elementRef,
+    name,
+    required,
+    validate,
+    dependsOn,
+  });
+
+  useImperativeHandle(forwardedRef, () => field.fieldApiRef.current, [
+    field.fieldApiRef,
+  ]);
+
+  useImperativeHandle(field.ref, () => elementRef.current, []);
+
+  const dispatchChangeEvent = useCustomEventDispatcher<
+    TimePickerDomRef,
+    FieldEventDetail<any, any>
+  >({
+    ref: elementRef,
+    name: "field-change",
+    onEvent: onChange,
+  });
+
+  const dispatchSubmitEvent = useCustomEventDispatcher<
+    TimePickerDomRef,
+    FieldEventDetail<any, any>
+  >({
+    ref: elementRef,
+    name: "field-submit",
+    onEvent: onSubmit,
+  });
+
+  const formattedValue =
+    field.value != null
+      ? normalizeTimeValue(
+          durationToTime(field.value, timePickerProps.formatPattern ?? "HH:mm"),
+          timePickerProps.formatPattern ?? "HH:mm"
+        )
+      : undefined;
+
+  useEffect(() => {
+    if (field.value != null && field.value.split(":").length < 3) {
+      const normalized = normalizeTimeValue(
+        durationToTime(field.value, timePickerProps.formatPattern ?? "HH:mm"),
+        timePickerProps.formatPattern ?? "HH:mm"
+      );
+      field.fieldApiRef.current.setValue(normalized);
+    }
+  }, [field.value, timePickerProps.formatPattern]);
+
+  return (
+    <TimePicker
+      {...timePickerProps}
+      ref={elementRef}
+      name={field.name}
+      value={formattedValue}
+      readonly={props.readonly || field.isValidating || field.isSubmitting}
+      required={required}
+      valueState={field.valueState}
+      valueStateMessage={
+        !isErrorIgnored(field.error) &&
+        field.valueStateMessage != null && (
+          <div slot="valueStateMessage">{field.valueStateMessage}</div>
+        )
+      }
+      onChange={useEventCallback(async (event) => {
+        event.stopPropagation();
+
+        const value = event.detail.value;
+        field.fieldApiRef.current.setValue(value);
+        const valid = await field.fieldApiRef.current.validate();
+
+        dispatchChangeEvent({
+          name,
+          value,
+          valid,
+          field: field.fieldApiRef.current,
+          form: field.formApiRef.current,
+        });
+      })}
+      onSubmit={useEventCallback(async (event) => {
+        event.stopPropagation();
+
+        const value = field.fieldApiRef.current.getValue();
+        const valid = await field.fieldApiRef.current.validate();
+
+        dispatchSubmitEvent({
+          name,
+          value,
+          valid,
+          field: field.fieldApiRef.current,
+          form: field.formApiRef.current,
+        });
+      })}
+      onBlur={useEventCallback((event) => {
+        onBlur?.(event);
+        field.onBlur();
+      })}
+    />
+  );
+}) as <
+  FormValues extends FieldValues,
+  FormFieldName extends FieldPath<FormValues>
+>(
+  p: TimePickerFieldProps<FormValues, FormFieldName> & {
+    ref?: Ref<FormFieldRef<FormValues, FormFieldName>>;
+  }
+) => ReactElement;
