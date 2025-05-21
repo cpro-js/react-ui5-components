fc10b2f35e0c10e87d4e762402f5b8df86d46b3a
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-05-21 17:52:27 +0200

    test: currencyInputField required & prefilled & rounded & minTest

---
+++ packages/form/src/field/CurrencyInputField.test.stories.tsx
@@ -0,0 +1,136 @@
+import { action } from "@storybook/addon-actions";
+import { expect } from "@storybook/jest";
+import { Meta, StoryObj } from "@storybook/react";
+import { fn, userEvent, waitFor, within } from "@storybook/test";
+
+import { FormController } from "../form/FormController";
+import { CurrencyInputField } from "./CurrencyInputField";
+import { FormViewer, useFormViewer } from "./FormViewer";
+
+interface FormData {
+  theNumber?: number;
+}
+
+const meta = {
+  title: "Form/Field/CurrencyInputField/Interactions",
+  component: CurrencyInputField,
+  parameters: {
+    form: {
+      initialValues: {},
+      onSubmit: action("form-submit"),
+    },
+  },
+  args: {
+    currency: "EUR",
+    onFocus: fn(),
+  },
+} satisfies Meta<typeof CurrencyInputField>;
+
+export default meta;
+
+type Story = StoryObj<typeof CurrencyInputField>;
+
+const mockSubmit = fn();
+
+const createPrefilledCurrencyTest = (initial: number): Story => ({
+  render: (props, context) => {
+    const { submittedValues, handleSubmit } = useFormViewer<FormData>({
+      onSubmit: mockSubmit,
+    });
+    return (
+      <FormController
+        initialValues={{ theNumber: initial }}
+        onSubmit={handleSubmit}
+      >
+        <CurrencyInputField {...props} name="theNumber" />
+        <FormViewer submittedValues={submittedValues} />
+      </FormController>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const submitBtn = canvas.getByText("Submit");
+    await userEvent.click(submitBtn);
+    await waitFor(() => {
+      expect(mockSubmit).toHaveBeenCalledWith(
+        { theNumber: initial },
+        expect.anything()
+      );
+    });
+  },
+});
+
+export const PrefilledTest = createPrefilledCurrencyTest(10.29);
+
+export const PrefilledAndRoundedTest = createPrefilledCurrencyTest(10.299);
+
+export const RequiredTest = {
+  render: (props, context) => {
+    const { submittedValues, handleSubmit } = useFormViewer<FormData>({
+      onSubmit: mockSubmit,
+    });
+
+    return (
+      <FormController onSubmit={handleSubmit}>
+        <CurrencyInputField
+          data-testid="currency-input"
+          {...props}
+          name={"theNumber"}
+          required
+        />
+        <FormViewer submittedValues={submittedValues} />
+      </FormController>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const submitBtn = canvas.getByText("Submit");
+
+    await userEvent.click(submitBtn);
+
+    await waitFor(() => {
+      const currencyInput = canvas.getByTestId("currency-input");
+      expect(currencyInput.getAttribute("value-state")).toBe("Negative");
+      expect(mockSubmit).not.toHaveBeenCalled();
+    });
+  },
+} satisfies Story;
+
+export const ValidationMinTest = {
+  args: {
+    min: 4,
+  },
+  render: (props, context) => {
+    const { submittedValues, handleSubmit } = useFormViewer<FormData>({
+      onSubmit: mockSubmit,
+    });
+
+    return (
+      <FormController onSubmit={handleSubmit}>
+        <CurrencyInputField
+          data-testid="min-input"
+          {...props}
+          name="theNumber"
+        />
+        <FormViewer submittedValues={submittedValues} />
+      </FormController>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const host = canvas.getByTestId("min-input") as HTMLElement;
+    const input = host.shadowRoot?.querySelector("input") as HTMLInputElement;
+
+    await userEvent.type(input, "1");
+    await userEvent.tab();
+
+    const submitBtn = canvas.getByText("Submit");
+    await userEvent.click(submitBtn);
+
+    await waitFor(() => {
+      expect(host.getAttribute("value-state")).toBe("Negative");
+      expect(mockSubmit).not.toHaveBeenCalled();
+    });
+  },
+} satisfies Story;
