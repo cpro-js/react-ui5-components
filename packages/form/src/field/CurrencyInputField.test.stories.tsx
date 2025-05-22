eff7b790e58a50035f3e74d39d5004a2e383ba20
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-05-22 10:14:08 +0200

    test: currencyInputField min max test

--- packages/form/src/field/CurrencyInputField.test.stories.tsx
+++ packages/form/src/field/CurrencyInputField.test.stories.tsx
@@ -23,6 +23,9 @@ const meta = {
   args: {
     currency: "EUR",
     onFocus: fn(),
+    onInput: fn(),
+    onBlur: fn(),
+    onChange: fn(),
   },
 } satisfies Meta<typeof CurrencyInputField>;
 
@@ -118,19 +121,72 @@ export const ValidationMinTest = {
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-
     const host = canvas.getByTestId("min-input") as HTMLElement;
     const input = host.shadowRoot?.querySelector("input") as HTMLInputElement;
 
     await userEvent.type(input, "1");
-    await userEvent.tab();
 
-    const submitBtn = canvas.getByText("Submit");
-    await userEvent.click(submitBtn);
+    //forced onChange event. To-Do: Trigger it without forcing it
+    input.dispatchEvent(new Event("change", { bubbles: true }));
 
     await waitFor(() => {
       expect(host.getAttribute("value-state")).toBe("Negative");
       expect(mockSubmit).not.toHaveBeenCalled();
+    });
+  },
+} satisfies Story;
+
+export const ValidationMinMaxTest = {
+  args: {
+    min: 4,
+    max: 10,
+  },
+  render: (props, context) => {
+    const { submittedValues, handleSubmit } = useFormViewer<FormData>({
+      onSubmit: mockSubmit,
+    });
+
+    return (
+      <FormController onSubmit={handleSubmit}>
+        <CurrencyInputField
+          data-testid="min-input"
+          {...props}
+          name="theNumber"
+        />
+        <FormViewer submittedValues={submittedValues} />
+      </FormController>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const host = canvas.getByTestId("min-input") as HTMLElement;
+    const input = host.shadowRoot?.querySelector("input") as HTMLInputElement;
+
+    await userEvent.type(input, "1");
+
+    //forced onChange event. To-Do: Trigger it without forcing it
+    input.dispatchEvent(new Event("change", { bubbles: true }));
+
+    await waitFor(() => {
+      expect(host.getAttribute("value-state")).toBe("Negative");
+      expect(mockSubmit).not.toHaveBeenCalled();
+    });
+
+    await userEvent.keyboard("{Control>}a{/Control}{Backspace}"); //clearing input
+    await userEvent.type(input, "11");
+    input.dispatchEvent(new Event("change", { bubbles: true }));
+
+    await waitFor(() => {
+      expect(host.getAttribute("value-state")).toBe("Negative");
+      expect(mockSubmit).not.toHaveBeenCalled();
+    });
+
+    await userEvent.keyboard("{Control>}a{/Control}{Backspace}"); //clearing input
+    await userEvent.type(input, "8");
+    input.dispatchEvent(new Event("change", { bubbles: true }));
+
+    await waitFor(() => {
+      expect(host.getAttribute("value-state")).toBe("None");
     });
   },
 } satisfies Story;
