db7cc68a9c72623d8e0d28b692d0926d1ba4cd59
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-06-27 10:39:39 +0200

    test: textInputComponent

--- packages/form/src/component/TextInput.test.stories.tsx
+++ packages/form/src/component/TextInput.test.stories.tsx
@@ -2,7 +2,7 @@ import { expect } from "@storybook/jest";
 import { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import { userEvent } from "@storybook/testing-library";
-import { waitFor, within } from "@testing-library/react";
+import { fireEvent, waitFor, within } from "@testing-library/react";
 
 import { TextInput } from "./TextInput";
 
@@ -28,18 +28,62 @@ export const onSubmitTest: Story = {
   play: async ({ canvasElement, args }) => {
     const canvas = within(canvasElement);
 
-    // Zugriff auf Shadow DOM innerhalb von <ui5-input>
     const host = canvas.getByTestId("text-input");
-    const input = host.shadowRoot?.querySelector("input") as HTMLInputElement;
+    const input = host.shadowRoot!.querySelector<HTMLInputElement>("input")!;
 
     input.focus();
 
     await userEvent.type(input, "Hello");
 
+    fireEvent.change(input); //gets only detected if onChange event really accurse
+
     await userEvent.keyboard("{Enter}");
 
     await waitFor(() => {
       expect(args.onSubmit).toHaveBeenCalled();
+      expect(args.onChange).toHaveBeenCalled();
+    });
+
+    fireEvent.change(input);
+    await userEvent.keyboard("{Enter}");
+
+    await waitFor(() => {
+      //onChange should only accure if value changed
+      //onSubmit always on enter pressed
+      expect(args.onSubmit).toHaveBeenCalledTimes(2);
+      expect(args.onChange).toHaveBeenCalledTimes(1);
+    });
+
+    await userEvent.type(input, "2345");
+    fireEvent.change(input);
+    await userEvent.keyboard("{Enter}");
+
+    await waitFor(() => {
+      expect(args.onSubmit).toHaveBeenCalledTimes(3);
+      expect(args.onChange).toHaveBeenCalledTimes(2);
+    });
+  },
+};
+
+export const onlyOnChangeTest: Story = {
+  args: {
+    value: "",
+  },
+  play: async ({ canvasElement, args }) => {
+    const canvas = within(canvasElement);
+
+    const host = canvas.getByTestId("text-input");
+    const input = host.shadowRoot!.querySelector<HTMLInputElement>("input")!;
+
+    input.focus();
+
+    await userEvent.type(input, "Hello");
+
+    fireEvent.change(input);
+
+    await waitFor(() => {
+      expect(args.onChange).toHaveBeenCalled();
+      expect(args.onSubmit).not.toHaveBeenCalled();
     });
   },
 };
