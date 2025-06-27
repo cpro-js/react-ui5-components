20599200e48b4974d9210e4a61744bda219da1f9
nwoellgens <nico.woellgens@cpro-conlog.com>
2025-06-27 12:35:35 +0200

    test: button component

---
+++ packages/form/src/component/Button.test.stories.tsx
@@ -0,0 +1,36 @@
+import { expect } from "@storybook/jest";
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { userEvent } from "@storybook/testing-library";
+import { fireEvent, waitFor, within } from "@testing-library/react";
+
+import { Button } from "./Button";
+
+export default {
+  title: "Component/Button/Interactions",
+  component: Button,
+  args: {
+    onFocus: fn(),
+    onBlur: fn(),
+    onClick: fn(),
+  },
+  render: (args) => <Button data-testid="button" {...args} />,
+} satisfies Meta<typeof Button>;
+
+type Story = StoryObj<typeof Button>;
+
+export const onClickTest = {
+  args: {
+    children: "Button",
+  },
+  play: async ({ canvasElement, args }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByTestId("button");
+
+    await userEvent.click(button);
+
+    await waitFor(() => {
+      expect(args.onClick).toHaveBeenCalled();
+    });
+  },
+} satisfies Story;
